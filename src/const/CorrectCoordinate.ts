b8999bfbd0b487acf4886ec3b9360e223bd1f208
Ali Kargar <aligol20@gmail.com>
2020-08-03 19:14:59 +0430

    Path fixed

--- src/const/CorrectCoordinate.ts
+++ src/const/CorrectCoordinate.ts
@@ -2,13 +2,13 @@
 export const correct_coordinate =
     [
 
-        { x: 0, y: 0, a: 1, image: require('./src/images/1.jpg') },
-        { x: 1, y: 0, a: 2, image: require('./src/images/2.jpg') },
-        { x: 2, y: 0, a: 3, image: require('./src/images/3.jpg') },
-        { x: 0, y: 1, a: 4, image: require('./src/images/4.jpg') },
-        { x: 1, y: 1, a: 5, image: require('./src/images/5.jpg') },
-        { x: 2, y: 1, a: 6, image: require('./src/images/6.jpg') },
-        { x: 0, y: 2, a: 7, image: require('./src/images/7.jpg') },
-        { x: 1, y: 2, a: 8, image: require('./src/images/8.jpg') },
-        { x: 2, y: 2, a: 9, image: require('./src/images/9.jpg') },
+        { x: 0, y: 0, a: 1, image: require('../images/1.jpg') },
+        { x: 1, y: 0, a: 2, image: require('../images/2.jpg') },
+        { x: 2, y: 0, a: 3, image: require('../images/3.jpg') },
+        { x: 0, y: 1, a: 4, image: require('../images/4.jpg') },
+        { x: 1, y: 1, a: 5, image: require('../images/5.jpg') },
+        { x: 2, y: 1, a: 6, image: require('../images/6.jpg') },
+        { x: 0, y: 2, a: 7, image: require('../images/7.jpg') },
+        { x: 1, y: 2, a: 8, image: require('../images/8.jpg') },
+        { x: 2, y: 2, a: 9, image: require('../images/9.jpg') },
     ]
