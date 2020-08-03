fb6d86b5e7c267f4c3200ff73ceaea3fa028b937
Ali Kargar <aligol20@gmail.com>
2020-08-03 19:15:09 +0430

    Image path fixed

--- src/const/ImageArray.ts
+++ src/const/ImageArray.ts
@@ -1,13 +1,13 @@
 // path of images
 
 export const image_array = [
-    require('./src/images/1.jpg'),
-    require('./src/images/2.jpg'),
-    require('./src/images/3.jpg'),
-    require('./src/images/4.jpg'),
-    require('./src/images/5.jpg'),
-    require('./src/images/6.jpg'),
-    require('./src/images/7.jpg'),
-    require('./src/images/8.jpg'),
-    require('./src/images/9.jpg')]
+    require('../images/1.jpg'),
+    require('../images/2.jpg'),
+    require('../images/3.jpg'),
+    require('../images/4.jpg'),
+    require('../images/5.jpg'),
+    require('../images/6.jpg'),
+    require('../images/7.jpg'),
+    require('../images/8.jpg'),
+    require('../images/9.jpg')]
 
