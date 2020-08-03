e679da62b79a8a3d055701c44f621e06aca8ef92
Ali Kargar <aligol20@gmail.com>
2020-08-03 19:07:56 +0430

    Correct coordinate for resolving the puzzle with a button

---
+++ src/const/CorrectCoordinate.ts
@@ -0,0 +1,14 @@
+// Correct coordinate for resolving the puzzle with a button
+export const correct_coordinate =
+    [
+
+        { x: 0, y: 0, a: 1, image: require('./src/images/1.jpg') },
+        { x: 1, y: 0, a: 2, image: require('./src/images/2.jpg') },
+        { x: 2, y: 0, a: 3, image: require('./src/images/3.jpg') },
+        { x: 0, y: 1, a: 4, image: require('./src/images/4.jpg') },
+        { x: 1, y: 1, a: 5, image: require('./src/images/5.jpg') },
+        { x: 2, y: 1, a: 6, image: require('./src/images/6.jpg') },
+        { x: 0, y: 2, a: 7, image: require('./src/images/7.jpg') },
+        { x: 1, y: 2, a: 8, image: require('./src/images/8.jpg') },
+        { x: 2, y: 2, a: 9, image: require('./src/images/9.jpg') },
+    ]
