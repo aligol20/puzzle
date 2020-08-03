153040baa6f3c418bbfe84334849dbb26d576a55
Ali Kargar <aligol20@gmail.com>
2020-08-03 19:08:04 +0430

    path of images

---
+++ src/const/ImageArray.ts
@@ -0,0 +1,13 @@
+// path of images
+
+export const image_array = [
+    require('./src/images/1.jpg'),
+    require('./src/images/2.jpg'),
+    require('./src/images/3.jpg'),
+    require('./src/images/4.jpg'),
+    require('./src/images/5.jpg'),
+    require('./src/images/6.jpg'),
+    require('./src/images/7.jpg'),
+    require('./src/images/8.jpg'),
+    require('./src/images/9.jpg')]
+
